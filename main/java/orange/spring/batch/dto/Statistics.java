62abfc8f3bb8f2d20a2a9cb7a8de13066844a595
orange <oltop@naver.com>
2022-02-10 17:02:06 +0900

    Update Statistics.java

--- main/java/orange/spring/batch/dto/Statistics.java
+++ main/java/orange/spring/batch/dto/Statistics.java
@@ -8,20 +8,12 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 public class Statistics {
-	private Long wbsSeqNo;
-	private Long actvtSeqNo;
-	private String wbsLvl6;
-	private String wbsLvl7;
-	private String wbsLvl7Cd;
-	private String quantUnit;
+	private Long aaaaa;
+	private Long bbbbb;
+	private String ccccc;
+	private String ddddd;
+	private String eeeee;
+	private String fffff;
 	private String zoneVal;
-	private String plnLchYmd;
-	private String plnCmpltYmd;
-	private int plnQuantVal;
-	private BigDecimal totalQuantVal; 
-	private BigDecimal actualQuantVal;
-	private BigDecimal quantVal;
-	private String camPos;
-	private String revitElement;
-	//private String actualYmd;
+
 }
